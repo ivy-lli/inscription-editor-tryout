47012c4c1110f82b5f6522164ed73f3831ddb356
Lukas Lieb <lukas.lieb@axonivy.com>
2022-11-23 08:14:21 +0100

    label input

--- src/components/tabs/NameTab.tsx
+++ src/components/tabs/NameTab.tsx
@@ -1,12 +1,24 @@
 import { Label } from '@radix-ui/react-label';
+import React from 'react';
 import './NameTab.css';
 
 const NameTab = () => (
   <div className='name-tab'>
+    <LabelInput label='Display name' htmlFor='displayName'>
+      <input className="input" type="text" id="displayName" defaultValue="" />
+    </LabelInput>
+    <LabelInput label='Description' htmlFor='description'>
+      <textarea className="input" id="description" defaultValue="" />
+    </LabelInput>
+  </div>
+)
+
+const LabelInput = (props: {label: string, htmlFor: string, children: JSX.Element}) => (
+  <div className='name-column'>
     <Label className="label" htmlFor="displayName">
-      Display name
+      {props.label}
     </Label>
-    <input className="input" type="text" id="displayName" defaultValue="" />
+    {props.children}
   </div>
 )
 
